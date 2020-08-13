fb51f44090cfe98665c31be331c068a0e1872d0b
Jomaree Lawsin <jclawsin88@gmail.com>
2020-08-13 11:59:13 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+### Starter Pack
+
+I made this to optimize my productivity when developing anything.
+
+It has Gulp - a task runner that automates different tasks
+It has SASS/SCSS - a preprocessor for CSS to speed up styling
+It has Autoprefixer - automatically creates vendor prefixes 
+It has BrowserSync - live streams changes through the browser
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,37 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+
 
 // compile scss into css
+// add additional pipes below before the 'dest' pipe!
 function style() {
-    // 1. where is my scss file?
+    //  where is my scss file?
     return gulp.src('./scss/**/*.scss')
 
-    // 2 pass that file through sass compiler
+    // automatically compile vendor prefixes
+    .pipe(autoprefixer({ cascade: false }))
+
+    // pass that file through sass compiler
     .pipe(sass().on('error', sass.logError))
 
-    //3. where do I save the compiled css?
-    .pipe(gulp.dest('./css'))
+    // where do I save the compiled css?
+    .pipe(gulp.dest('css'))
 
-    //  4. stream changes to all browser
-    .pipe(browserSync.stream());
+    //  stream changes to all browser
+    .pipe(browserSync.stream())
+
 }
 
 function watch() {
     browserSync.init({
         server: {
+            // this saves it in the current directory
             baseDir: './'
         }
     });
+
     gulp.watch('./scss/**/*.scss', style);
     gulp.watch('./*.html').on('change', browserSync.reload);
     gulp.watch('./js/**/*.js').on('change', browserSync.reload);
